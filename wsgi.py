2e7d1fe9381903ff3761ae1ec34f22396170d3b6
mayank8566 <mayank84379@gmail.com>
2025-03-29 03:39:20 +0530

    addport

--- wsgi.py
+++ wsgi.py
@@ -19,4 +19,5 @@ spec.loader.exec_module(app_module)
 app = app_module.app
 
 if __name__ == "__main__":
-    app.run() 
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
